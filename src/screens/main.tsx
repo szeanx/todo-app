325d389240ddfe47f4f1cc9f1572a86c8781fa0a
szeanx <sean@relampagos.org>
2022-01-18 12:14:35 +0800

    Initial Commit

---
+++ src/screens/main.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import {
+  Text,
+  Box,
+  Center,
+  VStack,
+  themeTools,
+  useTheme,
+  useColorMode,
+  useColorModeValue,
+} from "native-base";
+import ThemeToggle from "../components/theme-toggle";
+import AnimatedCheckbox from "../components/animated-checkbox";
+
+export default function MainScreen() {
+  return (
+    <Center
+      _dark={{ bg: "blueGray.900" }}
+      _light={{ bg: "blueGray.50" }}
+      px={4}
+      flex={1}
+    >
+      <VStack space={5} alignItems="center">
+        <Box w="100px" h="100px">
+          <AnimatedCheckbox></AnimatedCheckbox>
+        </Box>
+        <Box p={10} bg={useColorModeValue("red.500", "yellow.500")}>
+          <Text>Hello World</Text>
+        </Box>
+        <ThemeToggle />
+      </VStack>
+    </Center>
+  );
+}
